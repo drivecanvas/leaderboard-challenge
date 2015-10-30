45eb222d62877e73f1d99405c3a06b4759a4be1a
gabe <gabriel.debeaupuis@gmail.com>
2015-10-30 16:02:51 -0700

    null_session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
     render json: { status: 404, code: exception.class.to_s, status: exception.to_s }, status: :not_found
